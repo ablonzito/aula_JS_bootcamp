a8ac87308012ddad8f3b5adfab33832ea9bad6c1
Ablonzito <silva_junior99@Hotmail.com>
2019-10-08 19:26:10 -0300

    InserindoArquivosDaAula

---
+++ main.js
@@ -0,0 +1,134 @@
+/*Aula02JS
+function Change(elemento){
+    console.log(elemento.value);
+}
+
+
+function load(){
+    alert("Pagina carregada");
+}
+
+
+function trocar(elemento){
+    // document.getElementById("mouseMove").innerHTML = "Obrigado por passar o mouse";
+    // alert("Parabens por passar o nmouse em cima =D");
+    elemento.innerHTML = "Obrigado por passar o mouse";
+}
+
+function voltar(elemento){
+    // document.getElementById("mouseMove").innerHTML = "Passe o mouse aqui";
+    elemento.innerHTML = "Passe o moouse denovo, so mais uma vez por favor ;-;";
+}
+
+
+
+function surpresa(){
+    window.open("http://www.google.com"); // abre uma aba nova
+    window.location.href = "http://www.youtube.com"; // Abre na mesma aba
+}
+
+
+function botao(){
+    document.getElementById("agradecimento").innerHTML = "<b>Obrigado por clicar</b>";
+
+    //console.log(document.getElementById("agradecimento"));
+    // alert("Obrigado por clicar");
+}
+*/
+
+
+
+
+
+
+/*function soma(n1, n2){
+    return n1 + n2;
+}
+
+function validarIdade(idade){
+    var validar;
+    if(idade >= 18){
+        validar = true;
+    }
+    else{
+        validar = false;
+    }
+    return validar;
+}
+
+var idade = prompt("Quantos anos voce tem:");
+console.log(validarIdade(idade));
+
+alert(soma(5,10));/*
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Aula01
+/*var nome = "Joao Silva";
+var idade = 20;
+var idade2 = 10;
+var frase = "Japão e o melhor time do mundo";
+alert(nome + " Tem " + idade);
+alert(idade + idade2);
+
+console.log(nome);
+console.log(idade + idade2);
+console.log(frase);
+console.log(frase.replace("Japão", "Brasil"));
+console.log(frase.toUpperCase());
+console.log(frase.toLowerCase());*/
+
+
+/*var lista = ["maca", "laranja", "abacaxi"];
+lista.push("uva"); // Inserir da array
+lista.pop("uva"); // remove da array
+console.log(lista.length); // Conta
+console.log(lista.toString()); // Tras como String ao invez de array
+console.log(lista.join(" | ")); // Insere entre itens da array
+var frutas = [{nome:"Uva", cor:"Roxa"}, {nome:"Laranja", cor:"Laranja"}];
+alert(frutas[1].nome);*/
+
+/*
+var idade = prompt("Qual sua idade");
+if (idade >= 18){
+    alert("Maior de idade");
+}
+else{
+    alert("Menor de idade")
+}*/
+
+/*var count = 0;
+while(count < 5){
+    console.log(count);
+    alert(count);
+    count++;
+}*/
+
+// var count;
+// for(count = 0; count < 5; count++){
+    // alert(count);
+// }
+
+// var d = new Date();
+// alert(d.getMonth()+ 1);
+// alert(d.getDay());
+// alert(d.getHours());
+
+
